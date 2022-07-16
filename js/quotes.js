80769274ac6203d22ac20a0192c4b2cdcb19aa4f
Haena <kimhn200605@gmail.com>
2022-07-16 09:08:17 +0900

    add: quotes.js

---
+++ js/quotes.js
@@ -0,0 +1,43 @@
+const quotes = [
+	// 랜덤으로 뽑아낼 '명언 (quote) / 저자 (author)' 를 객체 형식으로 지정해서 배열에 저장
+	{
+		quote: "The way to get started is to quit talking and begin doing.",
+		author: "Walt Disney",
+	},
+	{
+		quote: "Life is what happens when you're busy making other plans.",
+		author: "John Lennon",
+	},
+	{
+		quote: "The world is a book and those who do not travel read only one page.",
+		author: "Saint Augustine",
+	},
+	{
+		quote: "Life is either a daring adventure or nothing at all.",
+		author: "Helen Keller",
+	},
+	{
+		quote: "To Travel is to Live",
+		author: "Hans Christian Andersen",
+	},
+	{
+		quote: "Only a life lived for others is a life worthwhile.",
+		author: "Albert Einstein",
+	},
+	{
+		quote: "You only live once, but if you do it right, once is enough.",
+		author: "Mae West",
+	},
+	{
+		quote: "Never go on trips with anyone you do not love.",
+		author: "Hemmingway",
+	},
+	{
+		quote: "We wander for distraction, but we travel for fulfilment.",
+		author: "Hilaire Belloc",
+	},
+	{
+		quote: "Travel expands the mind and fills the gap.",
+		author: "Sheda Savage",
+	},
+];
